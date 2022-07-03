99dc1a0540ecd126a82000eafc10f11a0e124121
lucychowchow <89259704+lucychowchow@users.noreply.github.com>
2022-07-03 23:12:17 +0800

    Add files via upload

--- lesson 1.py
+++ lesson 1.py
@@ -8,3 +8,29 @@ Created on Sun Jul  3 19:58:21 2022
 a=1;b=4
 print (f"a+b={a+b}")
 print ("\n")
+#print words f" "
+#print variable {variable_name}
+
+b=1e+3
+print (f"b={b}")
+print ("\n")
+
+sum=a+b
+print (f"a+b={sum}")
+print ("\n")
+
+n1 = input("enter num1: ")
+n2 = input("enter num2: ")
+print(f"n1 + n2 = {n1 + n2}")
+#input's datatype is string
+print(f"n1 + n2 = {int(n1) + int(n2)}")
+
+
+
+
+
+
+
+
+
+
